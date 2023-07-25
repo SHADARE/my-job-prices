9de32ef0f194511d7fa42783b2886c699ffb4876
Shadare Japheth <Shadare.J@bullseye.local>
2023-07-25 01:42:29 +0100

    update README

--- src/component/Price.js
+++ src/component/Price.js
@@ -5,7 +5,7 @@ const Price = () => {
   const [toggle, setToggle] = useState(false);
 
  return (
-  <div>
+  <div className="overall">
       <button className="firb" onClick={() => setToggle(!toggle)}>{toggle ? "Annually" : "Monthly"}</button>
     {toggle ? 
       <div className="main-container">
@@ -14,8 +14,8 @@ const Price = () => {
       <h1><span id="span-l">$</span>199.99</h1><hr></hr>
       <p>1 TB Storage</p><hr></hr>
       <p>5 Users allowed</p><hr></hr>
-      <p>send up to 5GB</p><hr></hr>
-      <button id="f-button">LEARN MORE</button>
+      <p>send up to 5GB</p><hr></hr> 
+      <button id="f-button" className="new">LEARN MORE</button>
     </section>
 
     <section id="sec" className="two">
@@ -24,16 +24,16 @@ const Price = () => {
       <p>5 TB Storage</p><hr></hr>
       <p>10 Users allowed</p><hr></hr>
       <p>send up to 15GB</p><hr></hr>
-      <button id="f-button">LEARN MORE</button>
+      <button id="f-button" className="loi">LEARN MORE</button>
     </section>
       
-    <section id="sec">
+    <section id="sec" className="first-basic">
     <h4>Master</h4>
       <h1><span id="span-l">$</span>399.99</h1><hr></hr>
       <p>10 TB Storage</p><hr></hr>
       <p>15 Users allowed</p><hr></hr>
       <p>send up to 25GB</p><hr></hr>
-      <button id="f-button">LEARN MORE</button>
+      <button id="f-button" className="new">LEARN MORE</button>
     </section>
     </div>
     : 
@@ -53,10 +53,10 @@ const Price = () => {
       <p>1 TB Storage</p><hr></hr>
       <p>2 Users allowed</p><hr></hr>
       <p>send up to 10GB</p><hr></hr>
-      <button id="f-button">LEARN MORE</button>
+      <button id="f-button" className="loi">LEARN MORE</button>
     </section>
       
-    <section id="sec">
+    <section id="sec" className="first-basic">
     <h4>Master</h4>
       <h1><span id="span-l">$</span>39.99</h1><hr></hr>
       <p>2 TB Storage</p><hr></hr>
